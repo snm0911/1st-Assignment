32c1a8a00d7f3aa550165af78de9f8a7d0e1a50e
snm0911 <31638935+snm0911@users.noreply.github.com>
2017-09-05 12:18:08 +0900

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -3,7 +3,7 @@ public class Assignment {
 	public static void main(String args[])
 	{
 		System.out.println("Student Number : 2014301030");
-		System.out.println("Name : ");
+		System.out.println("Name : 박상현");
 		System.out.println("Phone Number : 010-5509-2675");
 		System.out.println("GitHub address : https://github.com/snm0911");
 	}
