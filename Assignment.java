854929ba210774cb8783ade9cfda63d6755cfe2f
snm0911 <31638935+snm0911@users.noreply.github.com>
2017-09-05 12:15:14 +0900

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,10 @@
+
+public class Assignment {
+	public static void main(String args[])
+	{
+		System.out.println("Student Number : 2014301030");
+		System.out.println("Name : ¹Ú»óÇö");
+		System.out.println("Phone Number : 010-5509-2675");
+		System.out.println("GitHub address : https://github.com/snm0911");
+	}
+}
